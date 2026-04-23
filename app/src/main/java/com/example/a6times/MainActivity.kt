e55dade244934f2f8da49c9ae1ed92f428fa6f4e
EGF09 <efege0906@gmail.com>
2026-04-23 03:36:07 +0300

    UI ve geçişleri oluşturuldu

---
+++ app/src/main/java/com/example/a6times/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.a6times
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import androidx.annotation.IdRes
+import com.example.a6times.uinav.ForgotActivity
+import com.example.a6times.uinav.RegisterActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val registerButton = findViewById<Button>(R.id.KayıtOlButton)
+        registerButton.setOnClickListener {
+            val regIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(regIntent)
+        }
+        val forgotButton = findViewById<Button>(R.id.SifremiUnuttumButton)
+        forgotButton.setOnClickListener {
+            val forgotIntent = Intent(this, ForgotActivity::class.java)
+            startActivity(forgotIntent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/a6times/uinav/ForgotActivity.kt
@@ -0,0 +1,30 @@
+package com.example.a6times.uinav
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a6times.R
+
+class ForgotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_forgot)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val approvePasswordButton = findViewById<Button>(R.id.ChangePasswordButton)
+        approvePasswordButton.setOnClickListener {
+            val approveIntent = Intent(this, com.example.a6times.uinav.ApprovePasswordActivity::class.java)
+            startActivity(approveIntent)
+            finish()
+        }
+    }
+}
